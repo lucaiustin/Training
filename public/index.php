a3bf7dbd5bfb9912f11ee42a9dfd7e7ce344a27b
Laurentiu Dodoloi <laurentiu.dodoloi@5psolutions.eu>
2019-11-26 12:40:39 +0200

    #4: index.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-iustin-luca/tasks/4 fixed commit issus.

--- public/index.php
+++ public/index.php
@@ -1,15 +1,17 @@
 <?php
-    require_once ("common.php");
+    require_once("../common.php");
 
     session_start();
-    //$_SESSION['cart'] = [];
     if (!isset($_SESSION["cart"])) {
         $_SESSION['cart'] = [];
     }
-
     $session_cart_ids = $_SESSION['cart'];
-
-    $stmt = $dbh->prepare('SELECT * FROM '. $table_name .' WHERE id NOT IN ('.implode(", ",$session_cart_ids).')');
+    $question_marks_array = array_fill(0, count($session_cart_ids), '?');
+    $question_marks_string = implode(", ", $question_marks_array);
+    $stmt = $dbh->prepare('SELECT * FROM products WHERE id NOT IN ('.$question_marks_string.')');
+    foreach ($session_cart_ids as $k => $id) {
+        $stmt->bindValue(($k+1), $id);
+    }
     $stmt->execute();
 
     if ($stmt !== FALSE) {
@@ -19,36 +21,36 @@
     }
 
     if (isset($_GET["id"])) {
-        $id = htmlspecialchars($_GET["id"]);
-        if (!in_array($id, $_SESSION['cart'])) {
-            array_push($_SESSION['cart'], htmlspecialchars($_GET["id"]));
+        if (filter_var($_GET["id"], FILTER_VALIDATE_INT) && !in_array($id, $_SESSION['cart'])) {
+                array_push($_SESSION['cart'], $_GET["id"]);
         }
         header('Location: index.php');
+        exit;
     }
 ?>
 
 <html>
-<head>
-    <title>Products</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
+    <head>
+        <title><?= translate('Products'); ?></title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
     <body>
-        <div class = "container">
-            <div class = "product-list">
+        <div class="container">
+            <div class="product-list">
                 <?php foreach($products as $product): ?>
-                    <div class = "product">
-                        <div class = "product-image">
-                             <img src = 'images/<?= $product['image_name']; ?>'>
+                    <div class="product">
+                        <div class="product-image">
+                             <img src="images/<?= $product["image_name"]; ?>">
                         </div>
-                        <div class = "product-info">
-                            <?= $product['id']; ?>
-                            <?= translate($product['title']); ?>
+                        <div class="product-info">
+                            <?= $product["id"]; ?>
+                            <?= $product["title"]; ?>
                         </div>
-                        <a href = '/index.php?id=<?= $product['id']; ?>'>Add</a>
+                        <a href="/index.php?id=<?= $product["id"]; ?>">Add</a>
                     </div>
                 <?php endforeach; ?>
             </div>
-            <a href = ''>Go to cart</a>
+            <a href=''>Go to cart</a>
         </div>
     </body>
 </html>
