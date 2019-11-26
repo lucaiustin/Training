6a0b826ce12c012cbe0f062aa40d388dc0216d5c
Laurentiu Dodoloi <laurentiu.dodoloi@5psolutions.eu>
2019-11-26 15:39:28 +0200

    #4: index.php http://pm.5psolutions.net/public/index.php?path_info=projects/training-iustin-luca/tasks/4 update validateInput() function

--- common.php
+++ common.php
@@ -11,8 +11,6 @@
     }
 
     function validateInput($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
+        $data = strip_tags($data);
         return $data;
     }
